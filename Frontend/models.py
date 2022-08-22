c574da06b5ce81e14fc41b97ffb36e2cdefe1013
Deepanshu Sharma <deepanshu2912001@gmail.com>
2022-08-22 20:03:17 +0530

    v+0.22.20

--- Frontend/models.py
+++ Frontend/models.py
@@ -1,3 +1,12 @@
+import email
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField( max_length=254)
+    psw = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=False)
+    
+    class Meta:
+        db_table = "user"
