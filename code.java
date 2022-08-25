d578be21c9639bde5ed2d67fa106d49051c37998
Deepanshu Sharma <deepanshu2912001@gmail.com>
2022-08-25 21:02:53 +0530

    merge with backend

---
+++ code.java
@@ -0,0 +1,21 @@
+class code
+{
+       public static void main(String args[])
+
+       {
+
+            // var i="";
+
+            // for (var a : args) {
+
+            //     i=a;
+
+            // }
+
+            System.out.println("Hello");
+
+            System.out.println("Hello");
+
+       }
+
+}    
