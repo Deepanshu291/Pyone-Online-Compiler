0df81ecd4b7a9784ced312efa17ba8f479176d40
Deepanshu Sharma <deepanshu2912001@gmail.com>
2022-09-10 18:39:30 +0530

    v+0.10.09.18.39

--- DEV_Compiler/settings.py
+++ DEV_Compiler/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_$e#oc85m=4y0z(hrkr&qfjn)!iw+(27g9vkkx$(2kksv8*vvp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
