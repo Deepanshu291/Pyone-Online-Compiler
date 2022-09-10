e2409ef19937fe3344793cf1a47cbc0dd1acf2f7
Deepanshu Sharma <deepanshu2912001@gmail.com>
2022-09-10 19:37:54 +0530

    v+0.10.09.19.37

--- pyone/settings.py
+++ pyone/settings.py
@@ -24,8 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_$e#oc85m=4y0z(hrkr&qfjn)!iw+(27g9vkkx$(2kksv8*vvp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
+# ALLOWED_HOSTS =[]
 ALLOWED_HOSTS = ['127.0.0.1','pyone1.herokuapp.com']
 
 
